eace24a5e46cf1d802649e8e698813221a0725fe
Kaifee3 <kaifiazam138@gmail.com>
2025-08-02 00:54:36 +0530

    update

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './CSS/Adminpanel.css';
+import './CSS/AdminPanel.css';
 
 function AdminPanel() {
   const [tasks, setTasks] = useState([]);
